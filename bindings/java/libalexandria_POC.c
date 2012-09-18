e9276f45a31463f960b9a0746865b654bd0ba202
Tor E Hagemann <hagemt@rpi.edu>
2012-09-18 23:40:57 +0200

    Added license information (LGPL)

--- bindings/java/libalexandria_POC.c
+++ bindings/java/libalexandria_POC.c
@@ -1,3 +1,19 @@
+/*
+ *    This file is part of libalexandria.
+ *
+ *    libalexandria is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    libalexandria is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with libalexandria.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,8 +33,8 @@ JNICALL Java_libalexandria_POC_print(JNIEnv *env, jclass jc, jstring jstr)
 	#endif
 	laf_printi_();
 	/* TODO get to work with passed string */
-	// size_t len = strnlen(cstr, LAF_MAX_LEN);
-	// laf_print_(ctr, len);
+	//size_t len = strnlen(cstr, LAF_MAX_LEN);
+	//laf_print_(ctr, len);
 	/* Remember to release memory */
 	(*env)->ReleaseStringUTFChars(env, jstr, cstr);
 	fprintf(stderr, "CLASS: %p\n", jc);
--- laf_print.f
+++ laf_print.f
@@ -1,12 +1,30 @@
+C     -------------------------------------------------------------------------
+C     This file is part of libalexandria.
+C
+C     libalexandria is free software: you can redistribute it and/or modify
+C     it under the terms of the GNU Lesser General Public License as published by
+C     the Free Software Foundation, either version 3 of the License, or
+C     (at your option) any later version.
+C
+C     libalexandria is distributed in the hope that it will be useful,
+C     but WITHOUT ANY WARRANTY; without even the implied warranty of
+C     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C     GNU Lesser General Public License for more details.
+C
+C     You should have received a copy of the GNU Lesser General Public License
+C     along with libalexandria.  If not, see <http://www.gnu.org/licenses/>.
+C     -------------------------------------------------------------------------
+
+C     -------------------------------------------------------------------------
       SUBROUTINE laf_printi
-C     -------------------------------------------
-      COMPLEX I
-      I = CMPLX(0, 1)
-      print *,I
-C     -------------------------------------------
+C     -------------------------------------------------------------------------
+      print *,CMPLX(0,1)
+C     -------------------------------------------------------------------------
       END
 
+C     -------------------------------------------------------------------------
       SUBROUTINE laf_print(STR)
-C     -------------------------------------------
-C     -------------------------------------------
+C     -------------------------------------------------------------------------
+      print *,STR
+C     -------------------------------------------------------------------------
       END

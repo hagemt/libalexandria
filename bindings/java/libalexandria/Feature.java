34fb6abf904101d92ad94d317c8007739e25bfb1
Tor E Hagemann <hagemt@rpi.edu>
2012-09-22 03:03:18 +0200

    Simplified Feature class, whiteboard time

--- bindings/java/libalexandria/Feature.java
+++ bindings/java/libalexandria/Feature.java
@@ -1,15 +1,9 @@
 package libalexandria;
 
-public abstract class Feature<T extends Comparable> implements Comparable<Feature<T>>, Comparable<T> {
+public abstract class Feature<T extends Comparable> implements Comparable<Feature<T>> {
 	protected final T datum;
 
 	protected Feature(T t) {
 		datum = t;
 	}
-
-	public final int compareTo(Feature<T> f) {
-		return datum.compareTo(f.datum);
-	}
-
-	public abstract int compareTo(T t);
 }

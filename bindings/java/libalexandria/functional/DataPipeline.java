43d599a88a295a7317003dc08c2523482843f883
Tor E Hagemann <hagemt@rpi.edu>
2012-10-09 10:05:12 +0200

    Preliminary pipelining work

---
+++ bindings/java/libalexandria/functional/DataPipeline.java
@@ -0,0 +1,52 @@
+/*
+ *    This file is part of libalexandria.
+ *
+ *    libalexandria is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    libalexandria is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with libalexandria.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package libalexandria.functional;
+
+import java.nio.ByteBuffer;
+
+class DataPipeline<M extends Number, N extends Number> {
+	private boolean enabled;
+	private final ByteBuffer buffer;
+
+	public DataPipeline(ByteBuffer buffer) {
+		this.buffer = buffer;
+		this.enabled = isOpen();
+	}
+	
+	public int next(M m) {
+		// TODO Auto-generated constructor stub
+		return m.intValue();
+	}
+	
+	public N next() {
+		// TODO Auto-generated constructor stub
+		return null;
+	}
+	
+	public synchronized int remaining() {
+		return (buffer == null) ? 0 : buffer.remaining();
+	}
+	
+	public synchronized boolean isOpen() {
+		return (enabled && buffer != null && buffer.hasArray() && buffer.isDirect());
+	}
+
+	public boolean setMode(boolean state) {
+		enabled = state;
+		return isOpen();
+	}
+}

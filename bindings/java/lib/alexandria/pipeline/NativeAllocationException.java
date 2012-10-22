c0d94b48633fab6aa6b2ebbe4eeb5dd6c1afd017
Tor E Hagemann <hagemt@rpi.edu>
2012-10-22 19:30:02 +0200

    We now officially have our own exception... joy...

--- bindings/java/lib/alexandria/pipeline/NativeAllocationException.java
+++ bindings/java/lib/alexandria/pipeline/NativeAllocationException.java
@@ -1,7 +1,38 @@
+/*
+ *    This file is part of libalexandria.
+ *
+ *    libalexandria is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    libalexandria is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with libalexandria.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package lib.alexandria.pipeline;
 
 import java.io.IOException;
 
 public class NativeAllocationException extends IOException {
 	private static final long serialVersionUID = -3960122176317023786L;
+	private final Class<? extends Label> clazz;
+
+	public NativeAllocationException(Class<? extends Label> l) {
+		this(l, l.toString());
+	}
+
+	public NativeAllocationException(Class<? extends Label> l, String msg) {
+		super(msg);
+		clazz = l;
+	}
+
+	@Override
+	public String toString() {
+		return "class [" + clazz + "] tried doing bad things! " + super;
+	}
 }

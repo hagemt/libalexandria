349088aad3d6f6638896022249ddb7ed10ff3ebf
Tor E Hagemann <hagemt@rpi.edu>
2012-10-24 23:24:19 +0200

    Disable Android JNI build while it gets sorted out

--- glue/ConfigureNativeTarget.cmake
+++ glue/ConfigureNativeTarget.cmake
@@ -1,12 +1,11 @@
 function(configure_native_target target flags includes deps libs)
-  message(STATUS "configure_native_target '${target}' '${flags}' '${includes}' '${deps}' '${libs}'")
+  message(STATUS "Android.mk: '${target}' '${flags}' '${includes}' '${deps}' '${libs}'")
   # First set the SONAME, TODO remember PARENT_SCOPE?
   set(NATIVE_LIBRARY_SONAME ${target})
   # Prepare includes (TODO does this next line work?)
   include_directories(${includes})
   foreach(LA_INCLUDE_DIR ${includes})
-    file(GLOB LA_INCLUDES "${LA_INCLUDE_DIR}/*.h")
-    list(APPEND NATIVE_LIBRARY_INCLUDES "${LA_INCLUDES}")
+    list(APPEND NATIVE_LIBRARY_INCLUDES "${LA_INCLUDE_DIR}")
   endforeach(LA_INCLUDE_DIR)
   # Prepare flags for C/C++
   if(CMAKE_COMPILER_IS_GNUCC)
@@ -29,9 +28,12 @@ function(configure_native_target target flags includes deps libs)
 \$(call import-module,${LA_DEPENDENCY})")
   endforeach(LA_DEPENDENCY)
   # Link each library internally and externally
-  target_link_libraries(${NATIVE_LIBRARY_SONAME} ${libs})
   foreach(LA_LIBRARY ${libs})
     set(NATIVE_LIBRARY_LDLIBS "${NATIVE_LIBRARY_LDLIBS} -l${LA_LIBRARY}")
+    find_library(LA_LIBRARY_PATH ${LA_LIBRARY}
+      PATHS "${NATIVE_LIBRARY_SEARCH_PATH}"
+      DOC "Library path for lib${LA_LIBRARY}")
+    target_link_libraries(${NATIVE_LIBRARY_SONAME} ${LA_LIBRARY_PATH})
   endforeach(LA_LIBRARY ${libs})
   # Export the name of this library? TODO something else...
   list(APPEND NATIVE_LIBRARY_EXPORTS "${NATIVE_LIBRARY_SONAME}")

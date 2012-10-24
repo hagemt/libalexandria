18231446e4be6ba89be75b43a4d540cc43243f3a
Tor E Hagemann <hagemt@rpi.edu>
2012-10-25 00:22:59 +0200

    Patch up the messy bootstrapper

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
+# Author: Tor E Hagemann
+# Purpose: Bootstrap libalexandria build
 
+# Should always find the right directory
+SCRIPT_DIR="$(cd -P $(dirname $0) && pwd)"
+if [ ! -x "${SCRIPT_DIR}/$(basename $0)" ]; then
+	echo "Problem running '${SCRIPT_DIR}/$(basename $0)'"
+	echo "Please check that you can execute this file."
+	exit 1
+fi
+
+# Some important files
+CONF_FILE="${SCRIPT_DIR}/user.CMakeCache.txt"
+INFO_FILE="${SCRIPT_DIR}/release-build-info.txt"
+
+# Some important commands
 COMMAND_CMAKE=$(which cmake)
 COMMAND_GIT=$(which git)
-INFO_FILE="build-info.txt"
-CONF_FILE="user.CMakeCache.txt"
 
 # Check for cmake, be gentle with users
 if [ ! -x "${COMMAND_CMAKE}" ]; then
@@ -17,27 +30,24 @@ if [ -x "${COMMAND_GIT}" ]; then
 	echo "Found $(${COMMAND_GIT} --version)"
 	GIT_REPO=$(${COMMAND_GIT} rev-parse --show-toplevel)
 	GIT_HASH=$(${COMMAND_GIT} rev-parse -q HEAD)
-	echo "${GIT_HASH}" > "${INFO_FILE}"
-	if [ ! -f "release-${INFO_FILE}" ]; then
-		cp "${INFO_FILE}" "release-${INFO_FILE}"
+	echo "${GIT_HASH}" > "${SCRIPT_DIR}/build-info.txt"
+	if [ ! -f "${INFO_FILE}" ]; then
+		cp -v "${SCRIPT_DIR}/build-info.txt" "${INFO_FILE}"
 	fi
 fi
 
 # Default parameters, if absolutely necessary
-SOURCE_DIR=${GIT_REPO:-"$(cd \"$(dirname \"$0\")/..\" && pwd)"}
-SOURCE_TAG=${GIT_HASH:-"$(head -qn 1 "release-${INFO_FILE}")"}
+SOURCE_DIR=${GIT_REPO:-"${SCRIPT_DIR}/.."}
+SOURCE_TAG=${GIT_HASH:-"$(head -qn 1 \"${INFO_FILE}\")"}
+BUILD_GEN=${CMAKE_GENERATOR:-"Unix Makefiles"}
+BUILD_DIR="${SOURCE_DIR}/build-${SOURCE_TAG}"
 
 # Configuration
-BUILD_DIR="${SOURCE_DIR}/build-${SOURCE_TAG}"
-BUILD_GEN=${USE_GENERATOR:-"Unix Makefiles"}
-if [ -n "${BUILD_DEV}" ]; then
-	if [ ! -f "../${CONF_FILE}" ]; then
-		ln -v "${CONF_FILE}" ..
-	fi
-fi
 echo "Configuring ${SOURCE_DIR} (tag: ${SOURCE_TAG})"
-mkdir -v "${BUILD_DIR}"
-pushd "${BUILD_DIR}"
-${COMMAND_CMAKE} -G "${BUILD_GEN}" ..
-popd
-${COMMAND_CMAKE} --build "${BUILD_DIR}"
+if [ ! -e "${BUILD_DIR}" ]; then
+	mkdir -v "${BUILD_DIR}"
+fi
+cd "${BUILD_DIR}"
+${COMMAND_CMAKE} -C "${CONF_FILE}" -G "${BUILD_GEN}" ..
+# TODO ant build script for Java? IDK...
+#${COMMAND_CMAKE} --build "${BUILD_DIR}"
